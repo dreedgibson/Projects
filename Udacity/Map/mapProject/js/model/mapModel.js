07b932b8b5ca4d5b3f3fbfddb63bedb523ce59f8
dreedgibson <dennis.reed.gibson@gmail.com>
2017-03-27 17:24:51 -0500

    Cleanup

--- Udacity/Map/mapProject/js/model/mapModel.js
+++
@@ -1,9 +0,0 @@
-// model of individual locations
-var Bar = function(data) {
-	this.address = data.address;
-	this.lat = data.lat;
-	this.long = data.long;
-	this.name = data.name;
-	this.rating = data.rating;
-	this.website = data.url;
-}
